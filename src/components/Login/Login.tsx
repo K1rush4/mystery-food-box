6c48175bdbda4a9e8129330f0fba449cdab404e4
K1rush4 <gomzikovKD@mail.ru>
2024-06-21 11:46:57 +0500

    комит с проблемным окном логина

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,52 +1,69 @@
 import "./Login.css"
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
+import {userData} from "../../assets/data.ts";
 
 interface ILogin {
-    loginVisible: boolean;
-    setLoginVisible: (arg0: boolean) => void;
+  loginVisible: boolean;
+  setLoginVisible: (arg0: boolean) => void;
+  setIsLogin: (arg0: boolean) => void;
 }
 
-export default function Login({loginVisible, setLoginVisible}: ILogin) {
+export default function Login({loginVisible, setLoginVisible, setIsLogin}: ILogin) {
 
-    useEffect(() => {
-        const loginWindow = document.getElementById('loginWindowId');
-        if (loginWindow) {
-            loginWindow.className = `loginWindow ${loginVisible ? "is-active" : ""}`;
-        }
-    }, [loginVisible]);
+  const loginRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
-    function handleLoginClose() {
-        setLoginVisible(false)
+  useEffect(() => {
+    const loginWindow = document.getElementById('loginWindowId');
+    if (loginWindow) {
+      loginWindow.className = `loginWindow ${loginVisible ? "is-active" : ""}`;
     }
+  }, [loginVisible]);
 
-    return (
-        <>
-            <div id={"loginWindowId"}>
-                <div className={"loginCloseButton"}>
-                    <img src="public/images/close.svg" onClick={handleLoginClose}/>
-                </div>
-                <div className={"loginWrapper"}>
-                    <img className={"loginLogoImg"} src="public/images/logo.avif"/>
-                    <form className={"loginForm"}>
-                        <div>
-                            <label htmlFor="login"> Логин: </label>
-                            <input type="text" id="login"/>
-                        </div>
-                        <div>
-                            <label htmlFor="password">Пароль: </label>
-                            <input type="password" id="password"/>
-                        </div>
-                    </form>
-                    <div className={"loginLoginButton"}>
-                        <button>Войти</button>
-                    </div>
-                    <div className={"loginNoAccNoPass"}>Нет аккаунта? Создать</div>
-                    <div className={"loginNoAccNoPass"}>Забыли пароль? Восстановить</div>
+  function handleLoginClose() {
+    setLoginVisible(false);
+  }
 
-                </div>
+  //Todo отправка формы на сервер и получение токена
+  function handleLogin() {
+    userData.map((user) => {
+      if (loginRef.current!.value ==  user.login && passwordRef.current!.value ==  user.password) {
+        setIsLogin(true);
+        setLoginVisible(false);
+      }
+    })
+  }
+
+
+
+  return (
+    <>
+      <div id={"loginWindowId"}>
+        <div className={"loginCloseButton"}>
+          <img src="../../../public/images/close.svg" onClick={handleLoginClose}/>
+        </div>
+        <div className={"loginWrapper"}>
+          <img className={"loginLogoImg"} src="../../../public/images/logo.avif"/>
+          <form className={"loginForm"}>
+            <div>
+              <label htmlFor="login"> Логин: </label>
+              <input type="text" id="login" ref={loginRef}/>
             </div>
-            <div className={`loginOverlay ${loginVisible ? "is-active" : ""}`}
-                 onClick={handleLoginClose}/>
-        </>
-    )
+            <div>
+              <label htmlFor="password">Пароль: </label>
+              <input type="password" id="password" ref={passwordRef}/>
+            </div>
+          </form>
+          <div className={"loginLoginButton"}>
+            <button type="submit" onClick={handleLogin}>Войти</button>
+          </div>
+          <div className={"loginNoAccNoPass"}>Нет аккаунта? Создать</div>
+          <div className={"loginNoAccNoPass"}>Забыли пароль? Восстановить</div>
+
+        </div>
+      </div>
+      <div className={`loginOverlay ${loginVisible ? "is-active" : ""}`}
+           onClick={handleLoginClose}/>
+    </>
+  )
 }
