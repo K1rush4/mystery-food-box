c175417852c2f9f65501a440aadcb0d16355a08b
K1rush4 <gomzikovKD@mail.ru>
2024-09-23 11:11:56 +0500

    Работает, победил troubles с паролями к sql

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express')
 const sequelize = require('./db');
 const models = require('./models/models')
-
 const PORT = process.env.PORT
 
 const app = express()
